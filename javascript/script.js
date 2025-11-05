34d674e56ad2efb5d1c4689161551a75514aca2b
nitish yadav <yadavmunnakumar951@gmail.com>
2025-11-05 16:04:58 +0530

    run the node in the terminal and changes the webpage using js

--- javascript/script.js
+++ javascript/script.js
@@ -2,3 +2,19 @@ alert("Hello, i am Developer.");
 
 console.log("code is running...")
 console.log("code is sucesfully...")
+
+var a = prompt("enter your number")
+var istrue = confirm("are you want to blast your computer?")
+
+if(istrue){
+    console.log("computer is blasting...")
+}
+
+else{
+    console.log("computer is not blasting...")
+}
+
+console.log("your number is " + a)
+
+document.title = "changing title through js"
+console.log("Boooommmmm now you see your title.")
