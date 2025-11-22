01e38a92489cb85de81593a7fe038e3f65e04545
nitish yadav <yadavmunnakumar951@gmail.com>
2025-11-22 18:19:16 +0530

    learn about conditions, opartors and turnary operator

---
+++ JS Conditionals/script.js
@@ -0,0 +1,22 @@
+console.log("this is conditinal lecture...")
+
+let age = 9;
+let grace = 2;
+
+console.log(age + grace);
+
+if((age + grace)>18){
+    console.log("you can drive");
+} 
+
+else {
+    console.log("you can not drive!");
+}
+
+//turnary oprator-->
+
+let a = 1;
+let b = 2;
+let c = 3;
+
+console.log("Largest Number: " + ((a>b)? a:(b>c)? b:c));
