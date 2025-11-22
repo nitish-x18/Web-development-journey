fb05efb3e940f24051e8e878bf811c8b30ae6c48
nitish yadav <yadavmunnakumar951@gmail.com>
2025-11-22 19:05:34 +0530

    learn about loops

---
+++ JS Loops/script.js
@@ -0,0 +1,22 @@
+console.log("i am a tutorial of loop...");
+
+//for loop-->
+// let a = 1;
+
+// for (let i = 0; i < 10; i++) {
+//     console.log(a + i);
+// }
+
+//while loop-->
+// let b = 1;
+// while(b<10){
+//     console.log(b);
+//     b++;
+// }
+
+//do-while loop-->
+let c = 0;
+do{
+    console.log(c + ". sorry");
+    c++;
+}while(c<=10);
