72fc9122740095f0310b2f4311c24c6767aff522
nitish yadav <yadavmunnakumar951@gmail.com>
2026-02-22 12:29:54 +0530

    learn about for in and for of loops

--- JS Loops/script.js
+++ JS Loops/script.js
@@ -15,8 +15,26 @@ console.log("i am a tutorial of loop...");
 // }
 
 //do-while loop-->
-let c = 0;
-do{
-    console.log(c + ". sorry");
-    c++;
-}while(c<=10);
+// let c = 0;
+// do{
+//     console.log(c + ". sorry");
+//     c++;
+// }while(c<=10);
+
+
+//for in loop---->
+let obj = {
+    name: "nitish",
+    roll_number: 24011010282,
+    class: "btech (CSE)",
+}
+
+for (const key in obj) {
+    const element = obj[key];
+    console.log(key,element);
+}
+
+//for of loop---->
+for (const character of "nitish") {
+    console.log(character);
+}
