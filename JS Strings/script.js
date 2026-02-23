c9bfb5bcdec7cc52c656372685510b0060f242b2
nitish yadav <yadavmunnakumar951@gmail.com>
2026-02-24 00:03:19 +0530

    explore about all strings functions

--- JS Strings/script.js
+++ JS Strings/script.js
@@ -1,7 +1,9 @@
 console.log("Hello this file is all about strings in the javascript...");
 
-let a = "nitish";
+let a = "Nitish";
 let name = "yadav";
+
+console.log(a.length);
 
 console.log(a[0]);
 console.log(a[1]);
@@ -13,3 +15,19 @@ console.log(a[6]);
 
 console.log("my name is " + a + " and surname is " + name );
 console.log(`my name is ${a} and surmane is ${name}.`);  //tempet litrals
+
+let b = `Roha"n`;
+console.log(b);
+
+console.log(a.length);
+console.log(a.toUpperCase());
+console.log(a.toLowerCase());
+console.log(a.slice(1,4));
+console.log(a.slice(1));
+
+console.log(a.replace("sh", "x18"));
+console.log(a.endsWith("sh"));
+console.log(a.startsWith("R"));
+
+let c = "     hello";
+console.log(c.trim());
