aecd233020a0c2db96e6ba40eaae22449994b713
nitish yadav <yadavmunnakumar951@gmail.com>
2026-02-23 23:37:37 +0530

    learn about templet litrals

--- JS Strings/script.js
+++ JS Strings/script.js
@@ -1,1 +1,15 @@
 console.log("Hello this file is all about strings in the javascript...");
+
+let a = "nitish";
+let name = "yadav";
+
+console.log(a[0]);
+console.log(a[1]);
+console.log(a[2]);
+console.log(a[3]);
+console.log(a[4]);
+console.log(a[5]);
+console.log(a[6]);
+
+console.log("my name is " + a + " and surname is " + name );
+console.log(`my name is ${a} and surmane is ${name}.`);  //tempet litrals
