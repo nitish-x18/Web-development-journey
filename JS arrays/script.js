af8fccac5e89e6bc3e7be436ddc8f619630305b4
nitish yadav <yadavmunnakumar951@gmail.com>
2026-02-26 23:37:55 +0530

    explore the functions of arrays in js

--- JS arrays/script.js
+++ JS arrays/script.js
@@ -1,6 +1,9 @@
 console.log("arrays in the javaScript----->");
 
 let arr = [1,2,3,4,5];
+let a1 = [1,3,2];
+let a2 = [6,7,8];
+let a3 = [9,10,11];
 
 console.log(arr, typeof arr);
 console.log(arr.length);
@@ -8,3 +11,19 @@ console.log(arr[2]);
 
 console.log(arr.toString());
 console.log(arr.join(" And "));
+
+arr.push(10);
+
+arr.pop();
+
+arr.push("kuchbhiaddkarsktehai");
+
+arr.shift();
+arr.unshift("RCb");
+
+delete arr[1];
+
+console.log(a1.concat(a2,a3));
+
+a1.sort();
+console.log(a1);
