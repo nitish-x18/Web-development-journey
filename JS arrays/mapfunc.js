9ba945191db2425c5b76b68d2897c3cfb3b49467
nitish yadav <yadavmunnakumar951@gmail.com>
2026-03-07 19:25:30 +0530

    learn about map function in arrays

---
+++ JS arrays/mapfunc.js
@@ -0,0 +1,16 @@
+let arr = [1,2,3,4,5];
+let newarr = [];
+
+// for (let index = 0; index < arr.length; index++) {
+//     const element = arr[index];
+//     newarr.push(element**2);
+// }
+
+// console.log(newarr);
+
+//oneliner for this map function---->
+let newarrr  = arr.map(element => {
+    return element**2;
+});
+
+console.log(newarrr);
