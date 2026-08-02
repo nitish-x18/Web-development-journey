104e1a3e53677922d2416ff268d111b876fc35b9
nitish yadav <yadavmunnakumar951@gmail.com>
2026-08-03 00:11:20 +0530

    use different types of middleware

--- Middleware in JS/main.js
+++ Middleware in JS/main.js
@@ -1,25 +1,44 @@
 const express = require('express');
+const fs = require('fs');
 const app = express();
+const birds = require("./routes/birds");
 const port = 3000;
 
 app.use(express.static("public")); //in-built Middleware
 
+app.use('/birds', birds);
+
+//application level middleware-1
+app.use((req, res, next) => {
+    console.log('Middleware 1')
+    req.spidy = "i am Spiderman"
+    fs.appendFileSync("login.txt", `${new Date(Date.now())} and method is used: ${req.method} Method\n`);
+    console.log(`${new Date(Date.now())} and method is used: ${req.method} Method`);
+    next();
+});
+
+//application middleware-2
+app.use((req,res,next) => {
+    console.log('Middleware 2')
+    next();
+})
+
 app.get('/', (req, res) => {
-  res.send('Hello World...');
+    res.send('Hello world... ' + req.spidy);
 });
 
 app.get('/about', (req, res) => {
-  res.send('Hello World... from the about page');
+    res.send('Hello World... from the about page');
 });
 
 app.get('/contact', (req, res) => {
-  res.send('Hello World... from the cotact page');
+    res.send('Hello World... from the cotact page');
 });
 
 app.get('/home', (req, res) => {
-  res.send('Hello World... from the home page');
+    res.send('Hello World... from the home page');
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+    console.log(`Example app listening on port ${port}`);
 });
