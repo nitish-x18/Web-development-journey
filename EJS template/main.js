bcf021343c5fb0188f6c1e0ab947c18ebc6863b0
nitish yadav <yadavmunnakumar951@gmail.com>
2026-08-03 23:39:38 +0530

    use ejs view template

---
+++ EJS template/main.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+    let siteName = "One8";
+    let searchText = "Search Now";
+    res.render('index', {siteName: siteName, searchText: searchText });
+});
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+});
